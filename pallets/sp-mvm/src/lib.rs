b96592a3b7af3cd3dbf8b5521ae4d558479f7e47
Alexander Koz <a@koz.email>
2020-12-22 07:07:43 +0300

    impl publish module; add tests; rewrite storage. (WiP)

--- pallets/sp-mvm/src/lib.rs
+++ pallets/sp-mvm/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(unused_imports)]
+#![allow(dead_code)]
 
 #[macro_use]
 extern crate log;
@@ -9,20 +10,18 @@ extern crate log;
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
 use frame_system::ensure_signed;
-use frame_support::dispatch::DispatchResult;
-
 use codec::{Encode, Decode};
+
+use move_core_types::value::MoveKind;
+use move_core_types::value::MoveKindInfo;
+use move_core_types::value::MoveTypeLayout;
 use move_vm_types::values::values_impl::Value;
 use move_core_types::language_storage::TypeTag;
 use move_vm::types::Gas;
 use move_vm::types::ScriptTx;
-use move_vm::data::{State, Storage};
 use sp_std::prelude::*;
-pub use move_vm::*;
-use move_vm::dvm::Dvm;
+use move_vm::Vm;
 use move_core_types::account_address::AccountAddress;
-use sp_runtime::print;
-use frame_support::debug;
 
 #[cfg(test)]
 mod mock;
@@ -30,12 +29,10 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-/// Create or get cached VM
-// fn get_vm() -> Dvm<StorageImpl<VMStorage>> {
-fn get_vm() -> impl move_vm::Vm {
-    let store: StorageImpl<VMStorage> = Default::default();
-    move_vm::dvm::Dvm::new(store)
-}
+pub mod mvm;
+pub mod storage;
+// pub mod error;
+// use error::*;
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
@@ -54,30 +51,8 @@ decl_storage! {
          // https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
 
          /// Storage for move- write-sets contains code & resources
-         VMStorage get(fn vmstorage): map hasher(blake2_128_concat) Vec<u8> => Option<Vec<u8>>;
+         pub VMStorage get(fn vmstorage): map hasher(blake2_128_concat) Vec<u8> => Option<Vec<u8>>;
      }
-}
-
-struct StorageImpl<T>(core::marker::PhantomData<T>);
-
-impl<T> Default for StorageImpl<T> {
-    fn default() -> Self {
-        Self(Default::default())
-    }
-}
-
-impl Storage for StorageImpl<VMStorage> {
-    fn get(&self, key: &[u8]) -> Option<Vec<u8>> {
-        VMStorage::get(key)
-    }
-
-    fn insert(&self, key: &[u8], value: &[u8]) {
-        VMStorage::insert(key, value)
-    }
-
-    fn remove(&self, key: &[u8]) {
-        VMStorage::remove(key)
-    }
 }
 
 pub trait AccountIdAsBytes<AccountId, T: Sized> {
@@ -105,10 +80,26 @@ where
         let mut result = [0_u8; LENGTH];
         let bytes = acc.encode();
 
-        result.iter_mut().enumerate().for_each(|(i, v)| {
-            *v = bytes.get(i).map(|v| *v).unwrap_or(0);
-        });
+        let skip = if bytes.len() < LENGTH {
+            LENGTH - bytes.len()
+        } else {
+            0
+        };
+
         result
+            .iter_mut()
+            .skip(skip)
+            .zip(bytes)
+            .for_each(|(dst, src)| {
+                *dst = src;
+            });
+        result
+    }
+}
+
+impl<T: Trait> Module<T> {
+    pub fn get_vm_storage() -> storage::VmStorageAdapter<VMStorage> {
+        Default::default()
     }
 }
 
@@ -129,97 +120,124 @@ decl_event!(
 decl_error! {
      pub enum Error for Module<T: Trait> {
           /// Error names should be descriptive.
-          NoneValue,
+        //   NoneValue,
           /// Errors should have helpful documentation associated with them.
           StorageOverflow,
 
           MoveScriptTxValidationError,
           MoveScriptTxExecutionError,
+          MoveModuleDeployError,
      }
 }
+
+// impl<T: Trait> From<VMError> for Error<T> {
+//     fn from(_: VMError) -> Self {
+//         todo!()
+//     }
+// }
 
 // Dispatchable functions allows users to interact with the pallet and invoke state changes.
 // These functions materialize as "extrinsics", which are often compared to transactions.
 // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 decl_module! {
      pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-          // Errors must be initialized if they are used by the pallet.
-          type Error = Error<T>;
+        // Errors must be initialized if they are used by the pallet.
+        type Error = Error<T>;
 
-          // Events must be initialized if they are used by the pallet.
-          fn deposit_event() = default;
+        // Events must be initialized if they are used by the pallet.
+        fn deposit_event() = default;
 
-          /// An example dispatchable that takes a singles value as a parameter, writes the value to
-          /// storage and emits an event. This function must be dispatched by a signed extrinsic.
-          #[weight = 10_000 + T::DbWeight::get().writes(1)]
-          // TODO: return DispatchResultWithPostInfo with spend gas by vm
-          pub fn execute(origin, script_code: Vec<u8>) -> dispatch::DispatchResult {
-              info!("Executing `execute`");
+        /// An example dispatchable that takes a singles value as a parameter, writes the value to
+        /// storage and emits an event. This function must be dispatched by a signed extrinsic.
+        #[weight = 10_000]
+        // TODO: return DispatchResultWithPostInfo with spend gas by vm
+        pub fn execute(origin, script_bc: Vec<u8>, args: Vec<u8>) -> dispatch::DispatchResult {
+            let who = ensure_signed(origin)?;
+            debug!("executing `execute` with signed {:?}", who);
+            // TODO: enable logger for tests
+            #[cfg(test)] eprintln!("executing `execute` with signed {:?}", who);
 
-              // Check that the extrinsic was signed and get the signer.
-              // This function will return an error if the extrinsic is not signed.
-              // https://substrate.dev/docs/en/knowledgebase/runtime/origin
-              let who = ensure_signed(origin)?;
-              info!("`ensure_signed` ok");
+            let vm = mvm::default_vm::<VMStorage>();
+            // TODO: gas-table & min-max values shoud be in genesis/config
+            let max_gas_amount = (u64::MAX / 1000) - 42;
+            // TODO: get native value
+            let gas_unit_price = 1;
+            let gas = Gas::new(max_gas_amount, gas_unit_price).unwrap();
 
-              let vm = get_vm();
+            let tx = {
+                let code: Vec<u8> = script_bc;
+                let args = Value::simple_deserialize(
+                    &args,
+                    &MoveKindInfo::Base(MoveKind::Copyable),
+                    &MoveTypeLayout::U64).map(|v|vec![v]).unwrap_or(vec!()
+                );
+                let type_args: Vec<TypeTag> = Default::default();
 
-              // TODO: gas-table & min-max values shoud be in genesis/config
-              let max_gas_amount = (u64::MAX / 1000) - 42;
-              // TODO: get native value
-              let gas_unit_price = 1;
-              let gas = Gas::new(max_gas_amount, gas_unit_price).unwrap();
+                let sender = T::account_as_bytes(&who);
+                debug!("converted sender: {:?}", sender);
+                #[cfg(test)] eprintln!("converted sender: {:?}", sender);
 
-              let tx = {
-                  let code: Vec<u8> = script_code;
-                  let args: Vec<Value> = Default::default();
-                  let type_args: Vec<TypeTag> = Default::default();
+                let senders: Vec<AccountAddress> = vec![
+                    AccountAddress::new(sender),
+                ];
 
-                  let sender = T::account_as_bytes(&who);
-                  {
-                      debug::info!("converted sender: {:?}", sender);
-                      #[cfg(feature = "std")]
-                      eprintln!("converted sender: {:?}", sender);
-                  }
+                ScriptTx::new(code, args, type_args, senders).map_err(|err|{
+                    Error::<T>::MoveScriptTxValidationError
+                })?
+            };
 
-                  let senders: Vec<AccountAddress> = vec![
-                      AccountAddress::new(sender),
-                  ];
+            let res = vm.execute_script(gas, tx);
+            debug!("execution result: {:?}", res);
+            #[cfg(test)] eprintln!("execution result: {:?}", res);
 
-                  ScriptTx::new(code, args, type_args, senders).map_err(|err|{
-                      // Error::<T>::MoveScriptTxValidationError(err.to_string())
-                      Error::<T>::MoveScriptTxValidationError
-                  })?
-              };
+            res.map_err(|err|{
+            // TODO: unwrap error
+            Error::<T>::MoveScriptTxExecutionError
+            })?;
 
-              let res = vm.execute_script(gas, tx);
-              #[cfg(feature = "std")]
-              eprintln!("result: {:?}", res);
+            // Emit an event:
+            // Self::deposit_event(RawEvent::ResourceMoved(unwrapped_res, who));
 
-              {
-                  debug::info!("execution result: {:?}", res);
-                  #[cfg(feature = "std")]
-                  eprintln!("execution result: {:?}", res);
-              }
+            // Return a successful DispatchResult
+            Ok(())
+        }
 
-              res.map_err(|err|{
-                // TODO: unwrap error
-                Error::<T>::MoveScriptTxExecutionError
-              })?;
+        #[weight = 10_000]
+        pub fn publish_module(origin, module_bc: Vec<u8>) -> dispatch::DispatchResult {
+            let who = ensure_signed(origin)?;
+            debug!("executing `publish` with signed {:?}", who);
+            #[cfg(test)] eprintln!("executing `publish` with signed {:?}", who);
 
-              // TODO: update storage:
-              // VMStorage::insert(...);
+            let vm = mvm::default_vm::<VMStorage>();
+            // TODO: gas-table & min-max values shoud be in genesis/config
+            let max_gas_amount = (u64::MAX / 1000) - 42;
+            // TODO: get native value
+            let gas_unit_price = 1;
+            let gas = Gas::new(max_gas_amount, gas_unit_price).unwrap();
 
-              // Emit an event:
-              // Self::deposit_event(RawEvent::ResourceMoved(unwrapped_res, who));
+            let tx = {
+                use move_vm::types::ModuleTx;
 
-              // Return a successful DispatchResult
-              Ok(())
-          }
+                let code: Vec<u8> = module_bc;
+                let sender = T::account_as_bytes(&who);
+                debug!("converted sender: {:?}", sender);
+                #[cfg(test)] eprintln!("converted sender: {:?}", sender);
 
-          // fn on_initialize(n: T::BlockNumber,) -> Weight { if n.into() == 42 { panic!("on_initialize") } 7 }
-          // fn on_finalize(n: T::BlockNumber,) { if n.into() == 42 { panic!("on_finalize") } }
-          // fn on_runtime_upgrade() -> Weight { 10 }
-          // fn offchain_worker() {}
+                ModuleTx::new(code, AccountAddress::new(sender))
+            };
+
+            let res = vm.publish_module(gas, tx);
+            debug!("publish result: {:?}", res);
+            #[cfg(test)] eprintln!("publish result: {:?}", res);
+
+            res.map_err(|err| Error::<T>::MoveModuleDeployError)?;
+
+            Ok(())
+        }
+
+        // fn on_initialize(n: T::BlockNumber,) -> Weight { if n.into() == 42 { panic!("on_initialize") } 7 }
+        // fn on_finalize(n: T::BlockNumber,) { if n.into() == 42 { panic!("on_finalize") } }
+        // fn on_runtime_upgrade() -> Weight { 10 }
+        // fn offchain_worker() {}
      }
 }
