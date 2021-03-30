6fe9b68951651fae3b1997e9cb28da3b3982c2b7
Alexander Koz <a@koz.email>
2021-03-30 17:47:56 +0300

    [WiP] upgrade to substrate v3.0.0 (rebased on top of milestone-3 (6fca2e4))

--- runtime/build.rs
+++ runtime/build.rs
@@ -1,9 +1,9 @@
-use wasm_builder_runner::WasmBuilder;
+use substrate_wasm_builder::WasmBuilder;
 
 fn main() {
     WasmBuilder::new()
         .with_current_project()
-        .with_wasm_builder_from_crates("2.0.0")
+        // .with_wasm_builder_from_crates("3.0.0")
         .export_heap_base()
         .import_memory()
         .build()
