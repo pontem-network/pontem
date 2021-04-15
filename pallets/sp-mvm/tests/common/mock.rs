51363cf587d9a618761236ad81613bef008a2df6
Alexander Koz <a@koz.email>
2021-04-15 12:46:02 +0300

    code format

--- pallets/sp-mvm/tests/common/mock.rs
+++ pallets/sp-mvm/tests/common/mock.rs
@@ -13,7 +13,6 @@ use sp_runtime::traits::{BlakeTwo256, IdentityLookup};
 use sp_runtime::{testing::Header};
 use move_vm::data::Oracle;
 
-
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
