8cd7b500db95720c6c47bea62d5a993771433cdb
Dm. Yakushev <rig410@yandex.ru>
2021-04-29 14:36:20 +0300

    Added test for benchmarks.

--- pallets/sp-mvm/src/benchmarking.rs
+++ pallets/sp-mvm/src/benchmarking.rs
@@ -293,3 +293,94 @@ mod store {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests_composite::{ExtBuilder, Test};
+    use frame_support::assert_ok;
+
+    #[test]
+    fn publish_std() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_publish_std::<Test>());
+        });
+    }
+
+    #[test]
+    fn publish_empty_module() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_publish_empty_module::<Test>());
+        });
+    }
+
+    #[test]
+    fn publish_many_deps_module() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_publish_many_deps_module::<Test>());
+        });
+    }
+
+    #[test]
+    fn publish_s_module() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_publish_s_module::<Test>());
+        });
+    }
+
+    #[test]
+    fn publish_m_module() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_publish_m_module::<Test>());
+        });
+    }
+
+    #[test]
+    fn publish_l_module() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_publish_l_module::<Test>());
+        });
+    }
+
+    #[test]
+    fn execute_many_params() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_execute_many_params::<Test>());
+        });
+    }
+
+    #[test]
+    fn execute_store() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_execute_store::<Test>());
+        });
+    }
+
+    #[test]
+    fn execute_load() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_execute_load::<Test>());
+        });
+    }
+
+    #[test]
+    fn execute_store_event() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_execute_store_event::<Test>());
+        });
+    }
+
+    #[test]
+    fn execute_vec_input() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_execute_vec_input::<Test>());
+        });
+    }
+
+    #[test]
+    fn execute_execute_loop() {
+        ExtBuilder::default().build().execute_with(|| {
+            assert_ok!(test_benchmark_eexecute_loop::<Test>());
+        });
+    }
+}
