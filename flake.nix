5ee07a91e20c029ffa18b1fd9daf29406b5a1a5d
Vladimir Serov <me@cab404.ru>
2021-07-16 15:54:59 +0300

    Added Nix Flakes

--- flake.nix
+++ flake.nix
@@ -7,15 +7,20 @@
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
+
+    rocksdb = {
+      flake = false;
+      url = "github:facebook/rocksdb";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk, fenix }:
+  outputs = flake-args@{ self, nixpkgs, utils, naersk, fenix, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         fenixArch = fenix.packages.${system};
         rustTargets = fenixArch.targets;
-        llvmPackages = pkgs.llvmPackages_12;
+        llvmPackagesR = pkgs.llvmPackages_12;
 
         rustToolchain = fenixArch.stable;
         rustToolchainWasm = rustTargets.wasm32-unknown-unknown.latest;
@@ -27,45 +32,26 @@
 
       in {
 
-        # defaultPackage = naersk-lib.buildPackage (with pkgs; {
-        #   root = ./.;
-        #   src = ./.;
-
-        #   PROTOC = "${protobuf}/bin/protoc";
-        #   PROTOC_INCLUDE="${protobuf}/include";
-        #   LLVM_CONFIG_PATH="${llvmPackages.bintools}/bin/llvm-config";
-        #   LIBCLANG_PATH="${llvmPackages_11.clang-unwrapped.lib}/lib";
-        #   RUST_SRC_PATH = "${rustToolchain.rust-src}/lib/rustlib/src/rust/library/";
-
-        #   SKIP_WASM_BUILD = 1;
-        # });
-
-        # defaultApp = utils.mkApp {
-        #     drv = self.defaultPackage."${system}";
-        # };
-
         devShell =
         with pkgs; mkShell {
           buildInputs = [
             protobuf openssl pre-commit pkgconfig
-            llvmPackages.libclang.out
-            cargo-binutils
+            llvmPackagesR.clang
 
             (fenixArch.combine [
-              fenixArch.stable.completeToolchain
-              rustTargets.wasm32-unknown-unknown.stable.toolchain
-              # rustToolchainWasm.toolchain
+              (fenixArch.latest.withComponents [ "cargo" "clippy-preview" "llvm-tools-preview" "rust-std" "rustc" "rustc-dev" "rustfmt-preview" ])
+              rustTargets.wasm32-unknown-unknown.latest.toolchain
             ])
 
           ];
 
 #          SKIP_WASM_BUILD = 1;
           PROTOC = "${protobuf}/bin/protoc";
-          PROTOC_INCLUDE="${protobuf}/include";
-          LLVM_CONFIG_PATH="${llvmPackages.bintools}/bin/llvm-config";
-          LIBCLANG_PATH="${llvmPackages_11.clang-unwrapped.lib}/lib";
+          # PROTOC_INCLUDE="${protobuf}/include";
+          LLVM_CONFIG_PATH="${llvmPackagesR.llvm}/bin/llvm-config";
+          LIBCLANG_PATH="${llvmPackagesR.libclang.lib}/lib";
           RUST_SRC_PATH = "${rustToolchain.rust-src}/lib/rustlib/src/rust/library/";
-
+          ROCKSDB_LIB_DIR = "${rocksdb}/lib";
         };
 
       });
