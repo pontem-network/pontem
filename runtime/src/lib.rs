d90931219a70a9f064a0f5a0f792f0a33df5cbe8
borispovod <boris@wings.ai>
2021-10-21 18:08:32 +0300

    fix errors in esmiate rpc

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -746,7 +746,7 @@ impl_runtime_apis! {
         // Estimate gas for publish module.
         fn estimate_gas_publish(account: AccountId, module_bc: Vec<u8>, gas_limit: u64) -> Result<MVMApiEstimation, sp_runtime::DispatchError> {
             // TODO: pass real error.
-            let vm_result = Mvm::raw_publish_module(&account, module_bc, gas_limit, true).map_err(|_| sp_runtime::DispatchError::Other("error during VM execution"))?;
+            let vm_result = Mvm::raw_publish_module(&account, module_bc, gas_limit, true)?;
 
             Ok(MVMApiEstimation {
                 gas_used: vm_result.gas_used,
@@ -756,7 +756,7 @@ impl_runtime_apis! {
 
         // Estimate gas for execute script.
         fn estimate_gas_execute(account: AccountId, tx_bc: Vec<u8>, gas_limit: u64) -> Result<MVMApiEstimation, sp_runtime::DispatchError> {
-            let vm_result = Mvm::raw_execute_script(&[account], tx_bc, gas_limit, true).map_err(|_| sp_runtime::DispatchError::Other("error during VM execution"))?;
+            let vm_result = Mvm::raw_execute_script(&[account], tx_bc, gas_limit, true)?;
 
             Ok(MVMApiEstimation {
                 gas_used: vm_result.gas_used,
