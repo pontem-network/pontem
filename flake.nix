73c3a2006375e25dd0df051370d637ecadce11a8
Vladimir Serov <me@cab404.ru>
2021-11-01 23:44:09 +0000

    bumped flake

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@
   inputs = {
     fenix.url = github:nix-community/fenix;
     naersk.url = github:nmattia/naersk;
-    nixpkgs.url = github:NixOS/nixpkgs/staging-next;
     utils.url = github:numtide/flake-utils;
     move-tools.url = github:pontem-network/move-tools;
-#    naersk.inputs.nixpkgs.follows = "nixpkgs";
-#    fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixpkgs.follows = "move-tools/nixpkgs";
+    naersk.follows = "move-tools/naersk";
+    fenix.follows = "move-tools/fenix";
   };
 
   outputs = flake-args@{ self, nixpkgs, utils, naersk, fenix, move-tools, ... }:
@@ -62,7 +63,9 @@
             llvmPackagesR.clang
             dove
             devToolchain
-          ];
+          ] ++ (pkgs.lib.optionals stdenv.isDarwin [
+            libiconv darwin.apple_sdk.frameworks.Security
+          ]);
 
           SKIP_WASM_BUILD = "1";
           PROTOC = "${protobuf}/bin/protoc";
