5f7384477dd27ec0cacaf14835125e2b246d48c7
borispovod <boris@wings.ai>
2021-11-26 04:41:29 +0300

    runtime balances tests

--- runtime/src/tests/balances.rs
+++ runtime/src/tests/balances.rs
@@ -4,11 +4,16 @@ use frame_support::assert_ok;
 use sp_runtime::MultiAddress::Id as MultiId;
 use orml_traits::currency::MultiCurrency;
 
+fn get_unit(amount: Balance, currency_id: CurrencyId) -> Balance {
+    amount * u64::pow(10, currency_id.decimals() as u32)
+}
+
 #[test]
-fn transfer_native_currency_using_balances() {
+/// Test transfer native currency using Balances pallet.
+fn transfer_native_currency_via_balances() {
     let currency_id = GetNativeCurrencyId::get();
 
-    let initial_balance = 100 * PONT;
+    let initial_balance = get_unit(100, currency_id);
     let to_transfer = initial_balance / 2;
     let final_balance = to_transfer;
 
@@ -55,14 +60,25 @@ fn transfer_native_currency_using_balances() {
                 Tokens::free_balance(currency_id, &Accounts::ALICE.account()),
                 0
             );
+
+            assert_eq!(
+                Tokens::free_balance(currency_id, &Accounts::BOB.account()),
+                0
+            );
+
+            assert_eq!(
+                Balances::free_balance(&Accounts::BOB.account()),
+                to_transfer
+            );
         });
 }
 
 #[test]
-fn transfer_native_currency_using_currencies() {
+/// Test native currency transfer using Currencies pallet.
+fn transfer_native_currency_via_currencies() {
     let currency_id = GetNativeCurrencyId::get();
 
-    let initial_balance = 100 * PONT;
+    let initial_balance = get_unit(100, currency_id);
     let to_transfer = initial_balance / 2;
     let final_balance = to_transfer;
 
@@ -107,8 +123,138 @@ fn transfer_native_currency_using_currencies() {
             );
 
             assert_eq!(
-                Tokens::free_balance(currency_id, &Accounts::ALICE.account()),
+                Tokens::free_balance(currency_id, &Accounts::BOB.account()),
                 0
+            );
+
+            assert_eq!(
+                Balances::free_balance(&Accounts::BOB.account()),
+                to_transfer,
+            );
+        });
+}
+
+#[test]
+/// Test tokens transfer via Currencies pallet.
+fn transfer_tokens_via_currencies() {
+    let native_id = GetNativeCurrencyId::get();
+    let currency_id = CurrencyId::KSM;
+
+    let native_balance = get_unit(100, native_id);
+    let initial_balance = get_unit(100, currency_id);
+    let to_transfer = initial_balance / 2;
+    let final_balance = to_transfer;
+
+    RuntimeBuilder::new()
+        .set_balances(vec![(
+            Accounts::ALICE.account(),
+            CurrencyId::PONT,
+            native_balance,
+        ), (
+            Accounts::ALICE.account(),
+            CurrencyId::KSM,
+            initial_balance
+        )])
+        .build()
+        .execute_with(|| {
+            assert_eq!(
+                Tokens::free_balance(currency_id, &Accounts::ALICE.account()),
+                initial_balance
+            );
+
+            assert_eq!(
+                Currencies::free_balance(currency_id, &Accounts::ALICE.account()),
+                initial_balance
+            );
+
+            assert_ok!(Currencies::transfer(
+                Origin::signed(Accounts::ALICE.account()),
+                MultiId(Accounts::BOB.account()),
+                currency_id,
+                to_transfer,
+            ));
+
+            assert_eq!(
+                Currencies::free_balance(currency_id, &Accounts::ALICE.account()),
+                final_balance
+            );
+
+            assert_eq!(
+                Tokens::free_balance(currency_id, &Accounts::ALICE.account()),
+                final_balance
+            );
+
+            assert_eq!(
+                Tokens::free_balance(currency_id, &Accounts::BOB.account()),
+                to_transfer,
+            );
+
+            assert_eq!(
+                Currencies::free_balance(currency_id, &Accounts::BOB.account()),
+                to_transfer
+            );
+        });
+}
+
+
+#[test]
+/// Test tokens transfer via tokens pallet.
+fn transfer_tokens_via_tokens() {
+    let native_id = GetNativeCurrencyId::get();
+    let currency_id = CurrencyId::KSM;
+
+    let native_balance = get_unit(100, native_id);
+    let initial_balance = get_unit(100, currency_id);
+    let to_transfer = initial_balance / 2;
+    let final_balance = to_transfer;
+
+    RuntimeBuilder::new()
+        .set_balances(vec![(
+            Accounts::ALICE.account(),
+            CurrencyId::PONT,
+            native_balance,
+        ), (
+            Accounts::ALICE.account(),
+            CurrencyId::KSM,
+            initial_balance
+        )])
+        .build()
+        .execute_with(|| {
+            assert_eq!(
+                Tokens::free_balance(currency_id, &Accounts::ALICE.account()),
+                initial_balance
+            );
+
+            assert_eq!(
+                Currencies::free_balance(currency_id, &Accounts::ALICE.account()),
+                initial_balance
+            );
+
+            assert_ok!(Tokens::transfer(
+                Origin::signed(Accounts::ALICE.account()),
+                MultiId(Accounts::BOB.account()),
+                currency_id,
+                to_transfer,
+            ));
+
+            assert_eq!(
+                Currencies::free_balance(currency_id, &Accounts::ALICE.account()),
+                final_balance
+            );
+
+            assert_eq!(
+                Tokens::free_balance(currency_id, &Accounts::ALICE.account()),
+                final_balance
+            );
+
+            assert_eq!(
+                Tokens::free_balance(currency_id, &Accounts::BOB.account()),
+                to_transfer,
+            );
+
+            assert_eq!(
+                Currencies::free_balance(currency_id, &Accounts::BOB.account()),
+                to_transfer
             );
         });
 }
