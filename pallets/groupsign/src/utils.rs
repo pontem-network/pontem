afafb6667d4212a7bb39617a2c4a7088c24b7299
Vladimir Serov <me@cab404.ru>
2021-12-15 10:33:09 +0300

    cleaned up utils a bit

--- pallets/groupsign/src/utils.rs
+++ pallets/groupsign/src/utils.rs
@@ -3,41 +3,6 @@ use sp_io::{hashing::blake2_256};
 use sp_std::prelude::*;
 use frame_support::error::BadOrigin;
 
-#[cfg(feature = "benchmarking")]
-use frame_benchmarking;
-#[cfg(feature = "benchmarking")]
-use sp_core::{Pair, sr25519};
-#[cfg(feature = "benchmarking")]
-use sp_std::vec::Vec;
-
-// benchmark/test methods
-// each method works on reproducible keys
-#[cfg(feature = "benchmarking")]
-pub(crate) fn reencode<A: Encode, B: Decode>(a: A, buf: &mut [u8]) -> Result<B, codec::Error> {
-    a.using_encoded(|f| buf.copy_from_slice(f));
-    B::decode(&mut &buf[..])
-}
-#[cfg(feature = "benchmarking")]
-pub(crate) fn test_pairs(range: Range<u32>) -> impl Iterator<Item = sr25519::Pair> {
-    range.map(|acc| sr25519::Pair::from_entropy(&acc.to_be_bytes(), None).0)
-}
-#[cfg(feature = "benchmarking")]
-pub(crate) fn test_accounts<T: crate::Config>(range: Range<u32>) -> Vec<T::AccountId> {
-    test_pairs(range).map(|s| {
-        reencode(s.public(), &mut [0u8; 32]).expect("Decoded account")
-        // let mut buf = [0u8; 32];
-        // s.public().using_encoded(|f| buf.copy_from_slice(f));
-        // T::AccountId::decode(&mut &buf[..]).expect("Decoded account")
-    }).collect()
-}
-#[cfg(feature = "benchmarking")]
-pub(crate) fn test_sign<T: crate::Config>(range: Range<u32>, message: &[u8]) -> Vec<T::Signature>  {
-    test_pairs(range).map(|s| {
-        reencode(s.sign(message), &mut [0u8; 32]).expect("Decoded signature")
-        // let mut buf = [0u8; 32];
-        // s.sign(message).using_encoded(|f| buf.copy_from_slice(f));
-        // T::Signature::decode(&mut &buf[..]).expect("Decoded signature")
-    }).collect()
 /// Ensure this origin represents a groupsign origin
 pub fn ensure_groupsign<T, OuterOrigin>(o: OuterOrigin) -> Result<crate::Origin<T>, BadOrigin>
 where
