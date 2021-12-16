b2b9471e2fee71622cc645975aa506701914c467
tikhono <tikhonov.andrii.m@gmail.com>
2021-12-17 00:05:39 +0200

    compile errors fix

---
+++ pallets/transaction-pause/src/mock.rs
@@ -0,0 +1,130 @@
+#![cfg(test)]
+
+use super::*;
+use frame_support::{
+    construct_runtime, ord_parameter_types, parameter_types,
+    traits::{Everything, Nothing},
+};
+use frame_system::{EnsureSignedBy};
+use orml_traits::parameter_type_with_key;
+use primitives::{Amount, Balance, currency::CurrencyId};
+use sp_core::H256;
+use sp_runtime::{testing::Header, traits::IdentityLookup};
+use frame_support::traits::PalletInfo as OtherPalletInfo;
+
+pub type AccountId = u128;
+pub const ALICE: AccountId = 1;
+pub const AUSD: CurrencyId = CurrencyId::PONT;
+
+mod transaction_pause {
+    pub use super::super::*;
+}
+
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+}
+
+impl frame_system::Config for Runtime {
+    type BaseCallFilter = Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type Origin = Origin;
+    type Call = Call;
+    type Index = u64;
+    type BlockNumber = u64;
+    type Hash = H256;
+    type Hashing = ::sp_runtime::traits::BlakeTwo256;
+    type AccountId = AccountId;
+    type Lookup = IdentityLookup<AccountId>;
+    type Header = Header;
+    type Event = Event;
+    type BlockHashCount = BlockHashCount;
+    type DbWeight = ();
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
+    type OnSetCode = ();
+}
+
+parameter_types! {
+    pub const NativeTokenExistentialDeposit: Balance = 10;
+    pub const MaxReserves: u32 = 50;
+}
+
+impl pallet_balances::Config for Runtime {
+    type Balance = Balance;
+    type DustRemoval = ();
+    type Event = Event;
+    type ExistentialDeposit = NativeTokenExistentialDeposit;
+    type AccountStore = System;
+    type MaxLocks = ();
+    type MaxReserves = MaxReserves;
+    type ReserveIdentifier = ();
+    type WeightInfo = ();
+}
+
+parameter_type_with_key! {
+    pub ExistentialDeposits: |_currency_id: CurrencyId| -> Balance {
+        Default::default()
+    };
+}
+
+impl orml_tokens::Config for Runtime {
+    type Event = Event;
+    type Balance = Balance;
+    type Amount = Amount;
+    type CurrencyId = CurrencyId;
+    type WeightInfo = ();
+    type ExistentialDeposits = ExistentialDeposits;
+    type OnDust = ();
+    type MaxLocks = ();
+    type DustRemovalWhitelist = Nothing;
+}
+
+ord_parameter_types! {
+    pub const One: AccountId = 1;
+}
+
+impl Config for Runtime {
+    type Event = Event;
+    type UpdateOrigin = EnsureSignedBy<One, AccountId>;
+    type WeightInfo = ();
+}
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Runtime>;
+type Block = frame_system::mocking::MockBlock<Runtime>;
+
+construct_runtime!(
+    pub enum Runtime where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic
+    {
+        System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+        TransactionPause: transaction_pause::{Pallet, Storage, Call, Event<T>},
+        Balances: pallet_balances::{Pallet, Storage, Call, Event<T>},
+        Tokens: orml_tokens::{Pallet, Storage, Call, Event<T>},
+    }
+);
+
+pub struct ExtBuilder;
+
+impl Default for ExtBuilder {
+    fn default() -> Self {
+        ExtBuilder
+    }
+}
+
+impl ExtBuilder {
+    pub fn build(self) -> sp_io::TestExternalities {
+        let t = frame_system::GenesisConfig::default()
+            .build_storage::<Runtime>()
+            .unwrap();
+
+        t.into()
+    }
+}
