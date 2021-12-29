396b6ab2107d5c6f4d4ac89c2bf85cc7f174c90f
tikhono <tikhonov.andrii.m@gmail.com>
2021-12-29 19:44:34 +0200

    bad origin test + events emmit

--- runtime/src/tests/transation_pause.rs
+++ runtime/src/tests/transation_pause.rs
@@ -1,9 +1,12 @@
 /// Test balances in Runtime.
 use crate::tests::mock::*;
-use frame_support::{assert_ok, assert_err, dispatch::DispatchError};
+use frame_support::{assert_ok, assert_err, assert_noop, dispatch::DispatchError};
 
 use sp_runtime::{MultiAddress::Id as MultiId, traits::Dispatchable};
 use orml_traits::currency::MultiCurrency;
+
+use frame_system::{EventRecord, Phase};
+use transaction_pause::Event as PauseEvent;
 
 #[test]
 /// Test transfer native currency using Balances pallet.
@@ -67,5 +70,44 @@ fn transaction_pause_balance() {
                 Currencies::free_balance(currency_id, &Accounts::ALICE.account()),
                 initial_balance - to_transfer
             );
+            let events = System::events();
+            let mut events_iter = events.iter();
+            assert_eq!(
+                events_iter.next().unwrap(),
+                &EventRecord {
+                    phase: Phase::Initialization,
+                    event: Event::TransactionPause(PauseEvent::TransactionPaused(
+                        b"Balances".to_vec(),
+                        b"transfer".to_vec()
+                    )),
+                    topics: [].to_vec()
+                }
+            );
+            assert_eq!(
+                events_iter.next().unwrap(),
+                &EventRecord {
+                    phase: Phase::Initialization,
+                    event: Event::TransactionPause(PauseEvent::TransactionUnpaused(
+                        b"Balances".to_vec(),
+                        b"transfer".to_vec()
+                    )),
+                    topics: [].to_vec()
+                }
+            );
         });
+}
+
+#[test]
+/// Test exlusive root acces to pallet using Balances pallet.
+fn transaction_pause_bad_origin() {
+    RuntimeBuilder::new().build().execute_with(|| {
+        assert_noop!(
+            TransactionPause::pause_transaction(
+                Origin::signed(Accounts::BOB.account()),
+                b"Balances".to_vec(),
+                b"transfer".to_vec()
+            ),
+            BadOrigin
+        );
+    });
 }
