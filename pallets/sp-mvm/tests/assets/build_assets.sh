3c8d4a48a1f0495a3dbc999ccc9541f40744a1da
borispovod <boris@wings.ai>
2022-01-17 15:23:59 +0300

    build assets

--- pallets/sp-mvm/tests/assets/build_assets.sh
+++ pallets/sp-mvm/tests/assets/build_assets.sh
@@ -16,7 +16,7 @@ popd
 
 pushd ./user
 dove clean
-dove build --tree
+dove build
 dove build --package
 dove tx "store_u64(42)"
 dove tx "emit_event(42)"
@@ -25,11 +25,11 @@ dove tx "store_system_timestamp()"
 dove tx "inf_loop()"
 dove tx "store_native_balance()"
 dove tx "store_token_balance()"
-dove tx "transfer(treasury, 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY, 2000)"
-dove tx "transfer_token(treasury, 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY, 2000)"
-dove tx "multisig_test()"
-dove tx "deposit_bank<0x1::PONT::PONT>(2000)" -o=deposit_bank_pont.mvt
-dove tx "deposit_bank<0x1::KSM::KSM>(2000)" -o=deposit_bank_ksm.mvt
+dove tx "transfer<0x1::PONT::PONT>(@Alice, 2000)"
+#dove tx "transfer<PontemFrame>(treasury, gkQ5K6EnLRgZkwozG8GiBAEnJyM6FxzbSaSmVhKJ2w8FcK7ih, 2000)"
+#dove tx "multisig_test()"
+#dove tx "deposit_bank<0x1::PONT::PONT>(2000)" -o=deposit_bank_pont.mvt
+#dove tx "deposit_bank<0x1::KSM::KSM>(2000)" -o=deposit_bank_ksm.mvt
 #dove tx "store_native_deposit(false)"
 #dove tx "store_native_deposit(true)" -o=store_native_deposit_reg
 #dove tx "store_native_withdraw(false)"
