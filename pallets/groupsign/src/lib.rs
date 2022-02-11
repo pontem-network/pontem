d8b8fd40abea44eac1146cd3d43b51d7734e98be
Steve Degosserie <steve@parity.io>
2022-02-11 11:42:51 +0100

    SBP M2 review

--- pallets/groupsign/src/lib.rs
+++ pallets/groupsign/src/lib.rs
@@ -196,6 +196,7 @@ pub mod pallet {
                     ));
                     post_info.actual_weight
                 }
+                // SPB review: shouldn't the error be returned (w/ groupsign call weight added) ?
                 Err(err) => err.post_info.actual_weight,
             };
 
